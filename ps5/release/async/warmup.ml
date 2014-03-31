9986612d770520354799c5c9b233c8c630e55c6c
Humam Alwassel <haa54@cornell.edu>
2014-03-31 16:58:26 -0400

    finished fork in warmup.ml

--- ps5/release/async/warmup.ml
+++ ps5/release/async/warmup.ml
@@ -1,7 +1,7 @@
 open Async.Std
 
 let fork d f1 f2 =
-  failwith "My mind is aglow with whirling, transient nodes of thought..."
+	ignore (d >>= fun v -> Deferred.both (f1 v) (f2 v))
 
 let deferred_map l f =
   failwith "XQA"
