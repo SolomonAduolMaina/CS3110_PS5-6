1747bd974d1b4c6a8a34aefb9dd6a93857336133
Solomon Aduol Maina <sam524@cornell.edu>
2014-03-31 22:53:18 -0400

    added repo to eclipse workspace

--- ps5/release/async/warmup.ml
+++ ps5/release/async/warmup.ml
@@ -1,9 +1,9 @@
 open Async.Std
-
-let fork d f1 f2 =
-  ignore (d >>= fun v -> Deferred.both (f1 v) (f2 v))
-
-let deferred_map (l : 'a list) ( f : 'a -> 'b Deferred.t) =
-  List.fold_left 
-  (fun acc ele -> ele >>= fun v -> acc >>= fun v' -> return (v::v'))
-  (return []) (List.fold_left (fun acc ele -> (f ele)::acc) [] l)
+  
+let fork d f1 f2 = ignore (d >>= (fun v -> Deferred.both (f1 v) (f2 v)))
+  
+let deferred_map (l : 'a list) (f : 'a -> 'b Deferred.t) =
+  List.fold_left
+    (fun acc ele -> ele >>= (fun v -> acc >>= (fun v' -> return (v :: v'))))
+    (return []) (List.fold_left (fun acc ele -> (f ele) :: acc) [] l)
+  
