acc2e9ef6a709ca674bdd1945a3bfd7fea89daaa
Humam Alwassel <haa54@cornell.edu>
2014-03-31 22:59:53 -0400

    finished aQueue with testing -- no bugs and everything works

--- ps5/release/async/aQueue.ml
+++ ps5/release/async/aQueue.ml
@@ -2,20 +2,14 @@ open Async.Std
 open Pipe
 
 (* a pipe with a size *)
-type 'a t = 'a Reader.t * 'a Writer.t * int ref
+type 'a t = 'a Reader.t * 'a Writer.t
 
-let create () = let (r,w) = create () in (r, w, ref 0)    
+let create () : 'a t = Pipe.create ()   
 
-let push ((r, w, s): 'a t) x = 
-  s := !s + 1;
-  don't_wait_for (write w x)
+let push ((r, w): 'a t) x = don't_wait_for (write w x)
 
-let pop ((r, w, s): 'a t) = 
-  if !s = 0 then return (failwith "Empty Queue")
-  else begin
-    s := !s - 1;
-    read r >>= fun result -> match result with 
-      | `Eof -> failwith "<pop> EOF"
-      | `Ok x -> return x
-  end
-
+let pop ((r, w): 'a t) = 
+  read r >>= fun result -> 
+  match result with 
+    | `Eof -> failwith "<pop> EOF"
+    | `Ok x -> return x
--- ps5/release/async/testQueue.ml
+++ ps5/release/async/testQueue.ml
@@ -4,6 +4,15 @@ open Async.Std;;
 
 let test () = 
 let q = create () in 
+ignore (pop q >>= fun v -> print_endline (string_of_int v); return ());
+ignore (pop q >>= fun v -> print_endline (string_of_int v); return ());
+ignore (pop q >>= fun v -> print_endline (string_of_int v); return ());
+ignore (pop q >>= fun v -> print_endline (string_of_int v); return ());
+ignore (pop q >>= fun v -> print_endline (string_of_int v); return ());
+ignore (pop q >>= fun v -> print_endline (string_of_int v); return ());
+ignore (pop q >>= fun v -> print_endline (string_of_int v); return ());
+ignore (pop q >>= fun v -> print_endline (string_of_int v); return ());
+ignore (pop q >>= fun v -> print_endline (string_of_int v); return ());
 push q 1;
 push q 2;
 push q 3;
@@ -12,19 +21,10 @@ push q 5;
 push q 6;
 push q 7;
 push q 8;
-ignore (pop q >>= fun v -> print_endline (string_of_int v); return ());
-ignore (pop q >>= fun v -> print_endline (string_of_int v); return ());
-ignore (pop q >>= fun v -> print_endline (string_of_int v); return ());
-ignore (pop q >>= fun v -> print_endline (string_of_int v); return ());
-ignore (pop q >>= fun v -> print_endline (string_of_int v); return ());
-ignore (pop q >>= fun v -> print_endline (string_of_int v); return ());
-ignore (pop q >>= fun v -> print_endline (string_of_int v); return ());
-ignore (pop q >>= fun v -> print_endline (string_of_int v); return ());
-
-pop q >>= fun v -> print_endline (string_of_int v); return ();;
+push q 9;;
 
 (** Run the examples *)
-let _ = test () >>> fun () -> shutdown 0
+let _ = test () 
 
 (** Start the async scheduler *)
 let _ =
