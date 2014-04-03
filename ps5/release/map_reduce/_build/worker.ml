9c112c05cbaedbca008aa19047f4156588c71836
Solomon Aduol Maina <sam524@cornell.edu>
2014-04-03 12:44:42 -0400

    Set up request and response pipes. Attempted to write code for sending and receiving messages but get compilation error "unbound value WorkerRequest.receive. Probably has to do with file hierarchy. Will fix!"

---
+++ ps5/release/map_reduce/_build/worker.ml
@@ -0,0 +1,47 @@
+open Async.Std
+  
+open Reader
+  
+open Pipe
+
+open Protocol
+
+module Make (Job : MapReduce.Job) =
+  struct
+    (* Pipe for processing requests *)
+    let (reqreader, reqwriter) = Pipe.create ()
+      
+    (* Pipe for processing responses *)
+    let (resreader, reswriter) = Pipe.create ()
+      
+    (* Listens to controller and enques request onto request pipe if found *)
+    let queue_message r =
+      (WorkerRequest.receive r) >>=
+        (function
+         | `Eof -> return ()
+         | `Ok message -> write reqwriter message)
+      
+    let run r w = return (don't_wait_for (queue_message r))
+      
+  end
+  
+(* see .mli *)
+let init port =
+  (Tcp.Server.create ~on_handler_error: `Raise (Tcp.on_port port)
+     (fun _ r w ->
+        (read_line r) >>=
+          (function
+           | `Eof -> return ()
+           | `Ok job ->
+               (match MapReduce.get_job job with
+                | None -> return ()
+                | Some j -> let module Job = (val j)
+                    in let module Mapper = Make(Job) in Mapper.run r w))))
+    >>=
+    (fun _ ->
+       (print_endline "server started";
+        print_endline "worker started.";
+        print_endline "registered jobs:";
+        List.iter print_endline (MapReduce.list_jobs ());
+        never ()))
+  
--- ps5/release/map_reduce/worker.ml
+++ ps5/release/map_reduce/worker.ml
@@ -1,33 +1,47 @@
 open Async.Std
-
-module Make (Job : MapReduce.Job) = struct
-
-  (* see .mli *)
-  let run r w =
-    failwith "Nowhere special?  I always wanted to go there."
-
-end
-
+  
+open Reader
+  
+open Pipe
+  
+open Protocol
+  
+module Make (Job : MapReduce.Job) =
+  struct
+    (* Pipe for processing requests *)
+    let (reqreader, reqwriter) = Pipe.create ()
+      
+    (* Pipe for processing responses *)
+    let (resreader, reswriter) = Pipe.create ()
+      
+    (* Listens to controller and enques request onto request pipe if found *)
+    let queue_message r =
+      (WorkerRequest.receive r) >>=
+        (function
+         | `Eof -> return ()
+         | `Ok message -> write reqwriter message)
+      
+    let run r w = return (don't_wait_for (queue_message r))
+      
+  end
+  
 (* see .mli *)
 let init port =
-  Tcp.Server.create
-    ~on_handler_error:`Raise
-    (Tcp.on_port port)
-    (fun _ r w ->
-      Reader.read_line r >>= function
-        | `Eof    -> return ()
-        | `Ok job -> match MapReduce.get_job job with
-          | None -> return ()
-          | Some j ->
-            let module Job = (val j) in
-            let module Mapper = Make(Job) in
-            Mapper.run r w
-    )
-    >>= fun _ ->
-  print_endline "server started";
-  print_endline "worker started.";
-  print_endline "registered jobs:";
-  List.iter print_endline (MapReduce.list_jobs ());
-  never ()
-
-
+  (Tcp.Server.create ~on_handler_error: `Raise (Tcp.on_port port)
+     (fun _ r w ->
+        (read_line r) >>=
+          (function
+           | `Eof -> return ()
+           | `Ok job ->
+               (match MapReduce.get_job job with
+                | None -> return ()
+                | Some j -> let module Job = (val j)
+                    in let module Mapper = Make(Job) in Mapper.run r w))))
+    >>=
+    (fun _ ->
+       (print_endline "server started";
+        print_endline "worker started.";
+        print_endline "registered jobs:";
+        List.iter print_endline (MapReduce.list_jobs ());
+        never ()))
+  
