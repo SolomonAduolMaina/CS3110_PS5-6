3240e749e3cc3ce8945e48bd3858c4951ec647cf
Solomon Aduol Maina <sam524@cornell.edu>
2014-04-07 22:50:03 -0400

    Finished InvertedIndex app

--- ps5/release/apps/inverted_index/invertedIndex.ml
+++ ps5/release/apps/inverted_index/invertedIndex.ml
@@ -1,70 +1,91 @@
 open Async.Std
+  
 open Async_unix
-
+  
 type filename = string
 
 (******************************************************************************)
 (** {2 The Inverted Index Job}                                                *)
 (******************************************************************************)
-
-module Job = struct
-  type input
-  type key
-  type inter
-  type output
-
-  let name = "index.job"
-
-  let map input : (key * inter) list Deferred.t =
-    failwith "I'm stepping through the door / And I'm floating in a most peculiar way / And the stars look very different today"
-
-  let reduce (key, inters) : output Deferred.t =
-    failwith "Here am I floating round my tin can / Far above the Moon / Planet Earth is blue / And there's nothing I can do."
-end
-
+module Job =
+  struct
+    type file_name = string
+    
+    type contents = string
+    
+    type word = string
+    
+    type input = (file_name * contents)
+    
+    type key = word
+    
+    type inter = file_name
+    
+    type output = file_name list
+    
+    let name = "index.job"
+      
+    (* For each word in file filename, map that word to the filename *)
+    let map (filename, contents) : ((key * inter) list) Deferred.t =
+      let words = AppUtils.split_words contents in
+      let f word_list word = (word, filename) :: word_list
+      in return (List.fold_left f [] words)
+      
+    (* Remove duplicates from the list of filenames in which word key occurs *)
+    let reduce (key, inters) : output Deferred.t =
+      let module M = Map.Make(String)
+      in
+        let f table inter =
+          if M.mem inter table then table else M.add inter None table in
+        let g inter_list (inter, opt) = inter :: inter_list in
+        let pairs = M.bindings (List.fold_left f M.empty inters)
+        in return (List.fold_left g [] pairs)
+      
+  end
+  
 (* register the job *)
 let () = MapReduce.register_job (module Job)
-
-
+  
 (******************************************************************************)
 (** {2 The Inverted Index App}                                                *)
 (******************************************************************************)
-
-module App  = struct
-
-  let name = "index"
-
-  (** Print out all of the documents associated with each word *)
-  let output results =
-    let print (word, documents) =
-      print_endline (word^":");
-      List.iter (fun doc -> print_endline ("    "^doc)) documents
-    in
-
-    let sorted = List.sort compare results in
-    List.iter print sorted
-
-
-  (** for each line f in the master list, output a pair containing the filename
+module App =
+  struct
+    let name = "index"
+      
+    (** Print out all of the documents associated with each word *)
+    let output results =
+      let print (word, documents) =
+        (print_endline (word ^ ":");
+         List.iter (fun doc -> print_endline ("    " ^ doc)) documents) in
+      let sorted = List.sort compare results in List.iter print sorted
+      
+    (** for each line f in the master list, output a pair containing the filename
       f and the contents of the file named by f.  *)
-  let read (master_file : filename) : (filename * string) list Deferred.t =
-    Reader.file_lines master_file >>= fun filenames ->
-
-    Deferred.List.map filenames (fun filename ->
-      Reader.file_contents filename >>= fun contents ->
-      return (filename, contents)
-    )
-
-  module Make (Controller : MapReduce.Controller) = struct
-    module MR = Controller(Job)
-
-    (** The input should be a single file name.  The named file should contain
+    let read master_file : ((filename * string) list) Deferred.t =
+      (Reader.file_lines master_file) >>=
+        (fun filenames ->
+           Deferred.List.map filenames
+             (fun filename ->
+                (Reader.file_contents filename) >>=
+                  (fun contents -> return (filename, contents))))
+      
+    module Make (Controller : MapReduce.Controller) =
+      struct
+        module MR = Controller(Job)
+          
+        (** The input should be a single file name.  The named file should contain
         a list of files to index. *)
-    let main args =
-      failwith "Can you hear me, Major Zardoz? Can you hear me, Major Zardoz? Can you hear me, Major Zardoz?"
+        let main args =
+          match args with
+          | [] -> failwith "No file provided."
+          | [ x ] -> ((read (List.hd args)) >>= MR.map_reduce) >>| output
+          | _ -> failwith "Too many files provided"
+          
+      end
+      
   end
-end
-
+  
 (* register the App *)
 let () = MapReduce.register_app (module App)
-
+  
