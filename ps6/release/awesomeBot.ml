546f3d471937aee612be92ec0fdd4aab1b008e51
Humam Alwassel <haa54@cornell.edu>
2014-05-06 19:31:01 -0400

    finished everything and tested

--- ps6/release/awesomeBot.ml
+++ ps6/release/awesomeBot.ml
@@ -7,6 +7,14 @@ open BotUtil
 (** Give your bot a 2-20 character name. *)
 let name = "awesome_bot"
 
+(* stages refer to highest priority. If you can't meet this priority, then 
+try to do something else. E.g. in stage 0, if you can't upgrade a town  
+to a city, then try and build a road.  
+stage 0: convert 2 towns to 2 cities.
+stage 1: build roads to get longest road trophy
+stage 2: build an extra town.
+stage 3: convert third town to city.   
+stage 4: buy cards (largest army?).*)
 let stage = ref 0 
 (* the resources we care about the most in this stage. Update it whenever  *)
 (* you switch stages                                                       *)
@@ -15,7 +23,7 @@ let point_pieces = Hashtbl.create cNUM_POINTS
 let piece_hex = Hashtbl.create cNUM_PIECES
 let first_move = ref true
 let myColor = ref White
-
+let resources_from_first_town = ref []
 
 (* update resources_in_interest to include the types of resources needed for cost  *)
 let update_resources_in_interest cost = 
@@ -28,37 +36,80 @@ let update_resources_in_interest cost =
    the current stage is met, and updates the value of stage and 
    resources_in_interest if necessary.  *)
 let update_stage_and_resources_in_interest player_list inter_list = 
+(* stage 0: convert 2 towns to 2 cities. => resources_in_interest = cCOST_CITY
+stage 1: get longest road trophy.     => resources_in_interest = cCOST_ROAD
+stage 2: build an extra town.         => resources_in_interest = cCOST_TOWN
+stage 3: convert third town to city.  => resources_in_interest = cCOST_CITY
+stage 4: buy cards, get largest army. => resources_in_interest = cCOST_CARD *)
   let ((_, _, (_,longestroad, _)),_) = get_player (!myColor) player_list in
-  let two_cities = num_settlements (!myColor) City inter_list = 2 in
-  match (!stage), longestroad, two_cities with
-  | 0, _, true  -> stage := 1; update_resources_in_interest cCOST_ROAD
-  | 0, _, false -> ()
-  | 1, true, _  -> stage := 2; update_resources_in_interest cCOST_TOWN
-  | 1, false, _ -> ()
-  | 2, true, _  -> ()
-  | 2, false, _ -> stage := 1; update_resources_in_interest cCOST_ROAD
-  | _,_,_ -> ()
+  let num_cities = num_settlements (!myColor) City inter_list 
+  and num_towns = num_settlements (!myColor) City inter_list in
+  match (!stage), longestroad with
+  | 0, _ -> begin
+    if num_cities >= 2 
+    then (stage := 1; update_resources_in_interest cCOST_ROAD)
+    else (stage := 0; update_resources_in_interest cCOST_CITY)
+  end
+  | 1, true -> begin 
+    let s, r =
+      if num_cities >= 3 then (4, cCOST_CARD) 
+      else (if num_towns = 0 then (2, cCOST_TOWN) else (3, cCOST_CITY)) 
+    in
+    stage := s; update_resources_in_interest r
+  end
+  | _, false  -> stage := 1; update_resources_in_interest cCOST_ROAD
+  | 2, _      -> stage := 3; update_resources_in_interest cCOST_CITY
+  | 3, _      -> stage := 4; update_resources_in_interest cCOST_CARD
+  | _ -> ()
 
-let handle_InitialRequest (((map, structures, _, _, _), _, _, (c, _)) : state) : move = 
+
+
+(* first town: should be near adjacent to 3 pieces, and those 
+three adjacent pieces have high probability roll (i.e. 8=6>9=5>10=4>11=3>12=2).
+second town: should be next to a port with good trading discount ratio, and border 
+pieces with high probability roll. If possible, it should also be next to pieces 
+that generate resources that the first town can't obtain *)
+let rec handle_InitialRequest (((map, structures, _, _, _), _, _, (c, _)) as s : state) : move = 
   if !first_move then begin
     let () = first_move:= false in
     let () = myColor := c in
     let () = populate_piece_hex_hashtable piece_hex (fst map) in
     let options = get_first_town_options point_pieces piece_hex in
-    try 
-      let (p1, _) = List.find (fun (p, _) -> is_valid_town (fst structures) p) options in
-      InitialMove (p1, get_some (pick_random (adjacent_points p1)))
-    with _ -> InitialMove (Random.int cNUM_POINTS, Random.int cNUM_POINTS)
+    let p = 
+      (try 
+        fst (List.find (fun (p, _) -> is_valid_town (fst structures) p) options )
+      with _ -> Random.int cNUM_POINTS)
+    in
+    let lst = 
+      List.map (fun piece -> resource_of_terrain (fst (Hashtbl.find piece_hex piece))) (adjacent_pieces p)
+    in 
+    let () = resources_from_first_town := lst in 
+      InitialMove (p, get_some (pick_random (adjacent_points p)))
   end
   else begin
-    let options = get_second_town_options point_pieces piece_hex (snd map) in
-    let () = List.fold_left (fun () (p, (_, sum)) -> print_endline ("####(" ^ string_of_int p ^ ", " ^ string_of_float (sum *.36.) ^ ") ")) () options in 
+    let options = get_second_town_options point_pieces piece_hex (snd map) (!resources_from_first_town) 
+    in
+          let () = List.fold_left (fun () (p, (_, sum)) -> print_endline ("####(" ^ string_of_int p ^ 
+            ", " ^ string_of_float sum ^ ") ")) () options in 
     try
       let (p1, _) = List.find (fun (p, _) -> is_valid_town (fst structures) p) options in
       InitialMove (p1, get_some (pick_random (adjacent_points p1)))
-    with _ -> InitialMove (Random.int cNUM_POINTS, Random.int cNUM_POINTS)
+    with _ -> begin
+      first_move := true;
+      handle_InitialRequest s
+    end
   end
 
+(* RobberRequest: first thing make sure that you don't move the robber to 
+a piece next to our town or city. Robber should steal from the opponent 
+with the highest victory points at the time. calculate victory points by 
+checking towns and cities and trophies. If there is no unique opponent 
+with highest victory points, then pick the opponents that has more hidden 
+cards. once you identify a player to steal from, go look for a piece that 
+boards any of his settlements, and place the robber there. Make sure that 
+the piece you pick at the end is not a piece next to our settlements. 
+If you can't find any piece where this condition is satisfied, then pick 
+the opponent with the next highest victory points and do the same again. *)
 let handle_RobberRequest (((map, structures, _, _, robber), pl, _, _) : state) : move =
   let options = get_opponents_vpoints pl (!myColor) (fst structures) in 
   let rec helper = function
@@ -82,7 +133,9 @@ let handle_RobberRequest (((map, structures, _, _, robber), pl, _, _) : state) :
     in get_random_piece ()
   end 
 
-let handle_DiscradRequest (((_, _, _, _, _), pl, _, _) : state) : move =
+(* Discard request: look in the resources_in_interest list and try to keep
+  as much as you can of those and discard the other kinds of resources    *)
+let handle_DiscardRequest  (((_, _, _, _, _), pl, _, _) : state) : move =
   let ((_, ((b, w, o, g, l)as inv, _), _), _) = get_player (!myColor) pl in 
   let half = (sum_cost inv) / 2 in 
   let rec f out (count, kind) = 
@@ -95,9 +148,16 @@ let handle_DiscradRequest (((_, _, _, _, _), pl, _, _) : state) : move =
     List.partition (fun x -> List.mem x (!resources_in_interest)) all 
   in
   let in_intereset, not_in_interest = (randomize l1, randomize l2) in 
-  let cost = 
-    match in_intereset, not_in_interest with 
-    | _,_ -> (half,0,0,0,0) (* TODO*)
+  let rec helper out lst1 lst2 =
+    if sum_cost out = half then out 
+    else begin
+      match lst1, lst2 with 
+      | h::t, _ -> helper (plus_resources out (single_resource_cost h)) t lst2
+      | [], h::t -> helper (plus_resources out (single_resource_cost h)) lst1 t  
+      | _ -> (0,0,0,0,0)
+    end 
+  in
+  let cost = helper (0,0,0,0,0) not_in_interest in_intereset
   in
     DiscardMove cost
 
@@ -110,17 +170,18 @@ module Bot = functor (S : Soul) -> struct
     let () = Hashtbl.reset point_pieces in
     let () = populate_point_pieces_hashtable point_pieces in 
     let () = Hashtbl.reset piece_hex in
+    let () = resources_from_first_town := [] in
     ()
 
 
 	(* Invalid moves are overridden in game *)
   let handle_request ((b, p, t, (c, r)) as s : state) : move = 
     match r with
-      | InitialRequest -> handle_InitialRequest s
-      | RobberRequest -> handle_RobberRequest s
-      | DiscardRequest -> DiscardMove(0,0,0,0,0)
-      | TradeRequest -> TradeRequestBot.handle s (!stage)
-      | ActionRequest -> 
+      | InitialRequest  -> handle_InitialRequest s
+      | RobberRequest   -> handle_RobberRequest s
+      | DiscardRequest  -> handle_DiscardRequest s
+      | TradeRequest    -> TradeRequestBot.handle s (!stage)
+      | ActionRequest   -> 
         if is_none t.dicerolled then Action(RollDice) else Action(EndTurn)
 end
 
@@ -128,48 +189,3 @@ end
 (* Do not change *)
 let _ = register_bot name
   (module Bot(New)) (module Bot(New)) (module Bot(New)) (module Bot(New))
-
-
-(* stages refer to highest priority. if you can't meet this priority, then *)
-(* try to do something else. E.g. in stage 0, if you can't upgrade a town  *)
-(* to a city, then try and build a road stage 0: try to build another two  *)
-(* cities towns to cities. stage 1: build roads to get longest road trophy *)
-(* stage 2: build an extra town. stage 3: convert third town to city.      *)
-(* stage 4: buy cards (largest army?).                                     *)
-
-(* should have a function that updates the stage. I should be called every *)
-(* time we build a city or a road. it checks if we met the goal of this    *)
-(* stage, if so, then update the stage ref cell                            *)
-
-(* initialMove first town should be near to a 6 or 8 tile, has 3 adjacent  *)
-(* tiles, those three adjacent tiles are of more probable rolls (eg.       *)
-(* 8=6>9=5>10=4>11=3>12=2). and it should be next to tiles with road       *)
-(* building resources as much as possible second town: should be next to a *)
-(* port with good roll number and a trading discount ratio for the         *)
-(* resources that we have too much of from first town but we don't need    *)
-(* for building a road.                                                    *)
-
-(* RobberRequest: first thing make sure that you don't move the robber to  *)
-(* a tile next to our town or city next steal from someone who has more    *)
-(* points than the other 2 players. calculate winning points by checking   *)
-(* towns and cities and trophies. do this calculation for all the three    *)
-(* players, and sort by total points. If there is no equality on the       *)
-(* maximum, then pick the guy with the maximum. If there is equality       *)
-(* between two top players, then pick the guy with the most hidden cards   *)
-(* (more probable to have victory point cards).once you pick a guy, go     *)
-(* look for a tile that boards his settlements, and place the robber       *)
-(* there. Make sure that the tile you pick at the end is not a tile next   *)
-(* to our settlements. If you can't find any tile where this condition is  *)
-(* satisfied, then pick the next highest player and do the same again.     *)
-
-(* Discard request: look in the resources_in_interest list and try to keep *)
-(* as much as you can of those and discard the other kinds of resources    *)
-
-(* TradeRequest : have two functions enable_to_bulid() and is_fair()       *)
-(* enable_to_bulid re turns the bool value of "this trades will allow us   *)
-(* to build something (a town/city or a road)" is_fair() will return an    *)
-(* int that's equal to [resources we get] - [resources we give]. If        *)
-(* is_fair >=-1 then the trade is fair. so the logic for accepting a trade *)
-(* request is match enable_to_bulid(), is_fair () with | false, _ ->       *)
-(* reject | true, fair -> accept | true, not fair -> reject                *)
-
--- ps6/release/tradeRequestBot.ml
+++ ps6/release/tradeRequestBot.ml
@@ -7,17 +7,25 @@ open Constant
 open Util
   
 open BotUtil
-  
+
 let enables_to_build : cost -> int -> bool =
   fun inv stage ->
   let cost = match stage with
     | 0 -> cCOST_CITY
     | 1 -> cCOST_ROAD
-    | _ -> cCOST_TOWN
+    | 2 -> cCOST_TOWN
+    | 3 -> cCOST_CITY
+    | _ -> cCOST_CARD
   in 
     let (r1,r2,r3,r4,r5) = subtract_resources inv cost in
     r1>=0 && r2>=0 && r3>=0 && r4>=0 && r5>=0
   
+
+(* TradeRequest: have two functions enable_to_bulid and is_fair.       
+enable_to_bulid returns the value of "this trades will allow us   
+to build something (a town/city or a road)", and is_fair returns 
+true iff [resources we get] - [resources we give] >= -1. Accept 
+a trade iff enables_to_build && is_fair *)
 let handle : state -> int -> move =
   fun (board, plist, turn, (colour, _)) stage ->
     let ((c, (inv, hand), (ks, lr, la)), l) = get_player colour plist in
